06695a30f93b52e80daffea27c77dac4c4cd99ff
Alex M <mcgunn@oo2.be>
2020-05-12 11:41:43 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module file2configmap
+
+go 1.14
+
+require gopkg.in/yaml.v2 v2.2.8
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "log"
+
+  yaml "gopkg.in/yaml.v2"
+)
+
+type cmDefinition struct {
+  ConfigMaps []struct {
+    Name  string
+    Files []string
+  } `yaml:"configMaps"`
+}
+
+type kubernetesCM struct {
+  ApiVersion string `yaml:"apiVersion"`
+  Kind       string
+  Data       map[string]string
+  Metadata   struct {
+    Name string
+  }
+}
+
+func ParseConfig(fileName string) (*cmDefinition, error) {
+  data, err := ioutil.ReadFile(fileName)
+  if err != nil {
+    return nil, err
+  }
+  conf := &cmDefinition{}
+  err = yaml.Unmarshal(data, conf)
+  if err != nil {
+    return nil, err
+  }
+  return conf, nil
+}
+
+func BuildCM(conf *cmDefinition) (*[]*kubernetesCM, error) {
+  var manifests []*kubernetesCM
+  for _, cm := range conf.ConfigMaps {
+    m := getEmptyManifest()
+    m.Metadata.Name = cm.Name
+    for _, fileName := range cm.Files {
+      data, err := ioutil.ReadFile(fileName)
+      if err != nil {
+        return nil, err
+      }
+      m.Data[fileName] = string(data)
+    }
+    manifests = append(manifests, m)
+  }
+  return &manifests, nil
+}
+
+func PrintOutput(manifests *[]*kubernetesCM) error {
+  var outputs []string
+  for _, m := range *manifests {
+    out, err := yaml.Marshal(m)
+    if err != nil {
+      return err
+    }
+    outputs = append(outputs, string(out))
+  }
+  for _, o := range outputs {
+    fmt.Printf("---\n%+v", o)
+  }
+  return nil
+}
+
+func getEmptyManifest() *kubernetesCM {
+  return &kubernetesCM{
+    ApiVersion: "v1",
+    Kind:       "ConfigMap",
+    Data:       make(map[string]string),
+  }
+}
+
+func main() {
+  const fn = ".file2cm.yaml"
+  conf, err := ParseConfig(fn)
+  if err != nil {
+    log.Fatalf("Failed to read config from \"%v\": %v", fn, err)
+  }
+  mnf, err := BuildCM(conf)
+  if err != nil {
+    log.Fatalf("Failed to build manifest: %v", err)
+  }
+  err = PrintOutput(mnf)
+  if err != nil {
+    log.Fatalf("Failed to print manifest: %v", err)
+  }
+}
